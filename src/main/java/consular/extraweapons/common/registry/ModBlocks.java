e6554b6617592be76a77934967681553bc9a6962
ConsularAtol <83845237+ConsularAtol@users.noreply.github.com>
2021-08-15 16:09:44 -0400

    Update ModBlocks.java

--- src/main/java/consular/extraweapons/common/registry/ModBlocks.java
+++ src/main/java/consular/extraweapons/common/registry/ModBlocks.java
@@ -3,7 +3,7 @@ package consular.extraweapons.common.registry;
 public class ModBlocks {
 
     public static void registerBlocks(){
-        
+        //This is where new blocks WOULD be identified, if there were any.
     }
 
 }
