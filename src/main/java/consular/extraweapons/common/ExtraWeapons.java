9bf27f1121f973236c8e7e68e1977a10a72a7fb2
ConsularAtol <83845237+ConsularAtol@users.noreply.github.com>
2021-08-15 16:07:17 -0400

    Update ExtraWeapons.java

--- src/main/java/consular/extraweapons/common/ExtraWeapons.java
+++ src/main/java/consular/extraweapons/common/ExtraWeapons.java
@@ -1,1 +1,16 @@
+package consular.extraweapons.common;
 
+import consular.extraweapons.common.registry.ModBlocks;
+import consular.extraweapons.common.registry.ModItems;
+import net.fabricmc.api.ModInitializer;
+
+public class ExtraWeapons implements ModInitializer {
+
+	public static final String MODID = "extraweapons";
+
+	@Override
+	public void onInitialize() {
+		ModItems.registerItems();
+		ModBlocks.registerBlocks();
+	}
+}
