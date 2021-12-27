b0c54057412bb4e62df373e0d75190bef0cea5cf
ConsularAtol <83845237+ConsularAtol@users.noreply.github.com>
2021-12-27 15:42:00 -0500

    Comments

--- src/main/java/consular/extraweapons/common/ExtraWeapons.java
+++ src/main/java/consular/extraweapons/common/ExtraWeapons.java
@@ -1,6 +1,6 @@
 package consular.extraweapons.common;
 
-import consular.extraweapons.common.registry.ModBlocks;
+//import consular.extraweapons.common.registry.ModBlocks;
 import consular.extraweapons.common.registry.ModItems;
 import net.fabricmc.api.ModInitializer;
 
@@ -12,6 +12,7 @@ public class ExtraWeapons implements ModInitializer {
 	//I have the item and block registering code in seperate scripts, simply to make things cleaner.
 	public void onInitialize() {
 		ModItems.registerItems();
-		ModBlocks.registerBlocks();
+		//I was planning on adding blocks, but never did. But I'm commenting this out just in case.
+		//ModBlocks.registerBlocks();
 	}
 }
